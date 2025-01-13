c144df6a0fe1ad84374acdb2da90163b028fe300
jdragonhx <jayden.dhx@gmail.com>
2025-01-13 10:58:19 +0000

    Jan 13, 2025, 10:58 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -63,8 +63,8 @@ test1 = int(test1) #conversion function
 test2 = input("what is your test2 score? ")
 test2 = int(test2) #conversion function
 test3 = input("what is your test3 score? ")
-test3 = int(test2) #conversion function
-test4 = 88
+test3 = int(test3) #conversion function
+test3 = input("what is your test3 score? ")
 
 test1 = 0.25 * test1
 test2 = 0.25 * test2
