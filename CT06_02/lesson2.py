ac2bc3943ea106126395be2da8ebe5ef0032b879
jdragonhx <jayden.dhx@gmail.com>
2025-01-13 10:13:24 +0000

    Jan 13, 2025, 10:13 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -3,7 +3,7 @@ print("Hello from lesson 2")
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
 
-#
+#say hey!
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
