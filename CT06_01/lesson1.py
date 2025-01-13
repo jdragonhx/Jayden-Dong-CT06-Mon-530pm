e7e485b06cb4125272af8a935d31776929f76675
jdragonhx <jayden.dhx@gmail.com>
2025-01-13 09:56:18 +0000

    Jan 13, 2025, 9:56 AM

--- CT06_01/lesson1.py
+++ CT06_01/lesson1.py
@@ -11,3 +11,7 @@ print("Wake up and get out of bed.")
 print("Pour milk and cereal into a bowl.")
 print("Eat your breakfast.")
 #activite 9
+print("Hello and how are you?")
+print("What is wrong with this")
+print("Why is this not working?")
+Print("Why is this not working?")
