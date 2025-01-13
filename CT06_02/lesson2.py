9c0c05c3ba37d816f93937cfe85c75b75f2e0d57
jdragonhx <jayden.dhx@gmail.com>
2025-01-13 10:15:32 +0000

    Jan 13, 2025, 10:15 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -1,12 +1,12 @@
 print("Hello from lesson 2")
 
-######## Write the pseudocode in comments for task 2 here
+######## Write the pseudocode in comments for task 1a here
 # Using comments, translate the code shown on screen into pseudocode.
 
 #repeat 10 times
 #   say hey!
 #   move ten steps
 
-######## Write the pseudocode in comments for task 3 here
+######## Write the pseudocode in comments for task 1b here
 # Using comments, translate the code shown on screen into pseudocode.
 #
