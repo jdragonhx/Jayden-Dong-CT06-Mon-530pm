6354f06efa764ef71927fca7d237c1662ac5c2a6
jdragonhx <jayden.dhx@gmail.com>
2025-01-13 10:38:07 +0000

    Jan 13, 2025, 10:38 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -47,4 +47,4 @@ print("Hello from lesson 2")
 ######## Write the pseudocode in comments for task 1f here
 # Using comments, translate the code shown on screen into pseudocode.
 
-#
+#get the student test1 score
