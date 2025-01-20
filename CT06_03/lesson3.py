3043a1f5d8adad26ed371d3eca6e04d5ddcacbfa
jdragonhx <jayden.dhx@gmail.com>
2025-01-20 09:45:13 +0000

    Jan 20, 2025, 9:45 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -2,6 +2,6 @@
 # assign adam with score 60
 # assign brandon with score 58
 # assign Charlie with score 98
-# assign daniel 
+# assign daniel with score 79
 # take the sum , and divide by 4
-# print the final value
+# print the final 
