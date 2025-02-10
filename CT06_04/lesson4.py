1535dbe5f33673e7dacf27b114fc24e02729f275
jdragonhx <jayden.dhx@gmail.com>
2025-02-10 09:30:25 +0000

    Feb 10, 2025, 9:30 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,1 +1,79 @@
-print("Hello from lesson 4")
+# red = 1
+# blue = 2
+# green = 3
+
+# total = red * 3 + blue * 5 + green * 4
+# print (total)
+
+# reply = input("What is your name?")
+# print("hello " + reply)
+
+# reply = input("What is your favourite colour?")
+# print ("your faverout colour is " + reply)
+
+# reply = input("What is your age?")
+# print ("your age is " + reply)
+
+# reply = input ("what is your name")
+# counter = 500
+# print(counter * reply )
+
+# reply = input("Hi, what is your name?")
+# print ("hi, " + reply)
+
+# reply = input("whats your favourite hobby?")
+# print ("I enjoy " + reply + " too!")
+
+# reply = input("wheres your dream vacation?")
+# print ("I want to go to " + reply + " too!")
+
+# qty = 2
+# amt = 17.95
+# print("Total cost: $" , qty * amt)
+
+# age = int(input("what is your age? "))
+
+# num1 = 10
+# num2 = 5
+# ans = num1 + num2
+# print (str(num1) + "+" + str(num2) + "=" + str(ans))
+
+# age = input ("what is your age?")
+
+# print ("your age next year is: " + str(int(age) + 1))
+
+# year_born = input ("what year are you born in?")
+# year_born = int(year_born)
+# current_year = 2025
+# age = current_year - year_born
+# print ("this year, you are " + str(age) + " years old")
+
+#task 4
+# Assign the string "Hello" to a variable 'message'. 
+# Then reassign 'message' to the integer "10" and print it.
+
+# message = "hello"
+# message = 10
+# print(message)
+
+# task 5
+# Create 2 variables, 'firstName' and 'lastName', assigning your
+# first and last names to them. Then print them on the same line
+# with a space between them.
+
+# firstname = "jayden"
+# lastname = "Dong" 
+
+# print (firstname + " " + lastname)
+
+# Task 6
+# Assign a name to the variable "user_name"
+# Assign an integer to the variable "user_age"
+# Use type conversion and string concatenation to print out
+# "[user_name] is [user_age] years old."
+
+# user_name = input("Whats ur name?")
+# user_age = input("Whats ur age?")
+# user_age = int(user_age) 
+# user_name = int(user_name)
+# print(user_name + " is " + str(User_age))
--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,1 +1,28 @@
-print("Hello from lesson 5")
+# user_name = input("whats ur name")
+# user_new_age = input("how old are you turning this year?")
+# personal_msg = ("you are older now")
+# print( "(happy " + (user_new_age) +"th birthday " + (user_name) + "!" + personal_msg)
+
+
+# for count in range(100):
+#     print("teacher!")
+#     print("I will not repeat (teacher i broke it!)")
+
+# for abc in "I will not sling mud at my freind ever!":
+#     print (abc)
+
+# task 1
+# name = input("whats ur name?")
+# for abc in name:
+#     print("give me a " + abc)
+# print("what do we have here?")
+# print(name + "'s " + "the best!")
+
+# task 2
+# for rice in range(100):
+#     print(I like chicken rice!)
+
+#  task 3
+# for task in range(100):
+#     print("I like cake.")
+#     print("Give me more")
