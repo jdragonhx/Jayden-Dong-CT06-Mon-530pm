a9e0231c5d0a57b6094e0d751820c4ad8bb6312b
jdragonhx <jayden.dhx@gmail.com>
2025-02-10 10:37:31 +0000

    Feb 10, 2025, 10:37 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -26,3 +26,68 @@
 # for task in range(100):
 #     print("I like cake.")
 #     print("Give me more")
+
+# task 4
+# for letters in range(60):
+#     print(letters)
+
+
+## Task 5: range(start, stop)
+
+# **Task 5a**:
+# Print numbers from 1 to 5 using a 'for' loop.
+# for five in range(6):
+#     print(five)
+
+# **Task 5b**:
+# Print numbers from 51 to 100 using a 'for' loop.
+# for hundred in range(51, 101):
+#     print(hundred)
+
+# **Task 5c**:
+# Print numbers from 18 to 29 using a 'for' loop.
+# for num1 in range(18, 30):
+#     print(num1)
+
+## Task 6: range(start, stop, step)
+
+# **Task 6a**:
+# Use a 'for' loop to print numbers from 2 to 24 in multiples of 2.
+# for twos in range(2, 25, 2):
+#     print(twos)
+
+# **Task 6b**:
+# Use a 'for' loop to print numbers from 8 to 96 in multiples of 8.
+# for eights in range(8, 97, 8):
+#     print(eights)
+
+# **Task 6c**:
+# Use a 'for' loop to print numbers from 5 to 1 in descending order.
+# for task in range(5,0,-1):
+#     print(task)
+# print("BOO!")
+
+## Task 7: Countdown timer
+
+# **Task 7a**:
+# Imagine you are the race official and to start the race, you
+# must say the following:
+
+#     Ready!
+#     3
+#     2
+#     1
+
+# Using a 'for' loop, recreate the above sequence.
+
+# print("Ready!")
+# for race in range(3, 0, -1):
+#     print(race)
+
+
+
+# **Task 7b**:
+# Create a countdown timer that counts from 10 to 1.
+# When the timer hits 1, print "Boo!"
+
+----------------------------------------------------------------------------------------------------------------------------------------
