18f32389633d63da2de7bbb8e757a20bb8fab917
jdragonhx <jayden.dhx@gmail.com>
2025-02-10 10:42:14 +0000

    Feb 10, 2025, 10:42 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -92,3 +92,16 @@
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+## Task 8: User-Defined Range Counter
+
+# Using input(), ask the user for 2 numbers and store them in the
+# variables:
+# 1. start
+# 2. stop
+
+# Write a 'for' loop to count from **start** to **stop**
+
+# Note:
+# What happens if the user inputs a higher start number than stop?
+# Modify your code to be able to handle that scenario.
+
