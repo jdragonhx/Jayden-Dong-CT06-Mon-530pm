5582e469f3301353588d3772656abd4d3f983991
jdragonhx <jayden.dhx@gmail.com>
2025-02-24 09:52:50 +0000

    Feb 24, 2025, 9:52 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,2 +1,16 @@
-print("Hello from lesson 7")
+## Recap 1: Debugging Average Score Program
 
+### There is a total of 3 bugs in the following program.
+### Identify and fix the bugs:
+
+score_one = 80
+score_two = 90
+score_three = 75
+
+total = score_one + score_two 
+
+average_score = totl / 3
+
+student_name = "Alex"
+
+print("Average score for " + student_name + " is: " + average_score)
