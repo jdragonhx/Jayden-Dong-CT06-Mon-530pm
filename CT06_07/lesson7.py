b6feb4c758298331175d623bbb334cd1a55adfd3
jdragonhx <jayden.dhx@gmail.com>
2025-02-24 10:19:47 +0000

    Feb 24, 2025, 10:19 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -40,3 +40,17 @@
 #     print(i)
 
 
+## Task 4: Word Repetition Input Loop
+
+# Ask the user for a word and a number n. Print the word repeated
+# n times (on separate lines).
+
+# Example:
+# What word would you like to repeat? <>
+# How many times would you like to repeat? << 3 >>
+
+# output:
+# burger
+# burger
+# burger
+
