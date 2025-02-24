890cdac8be180b4a1f52c7729e05515b1d60c102
jdragonhx <jayden.dhx@gmail.com>
2025-02-24 10:14:25 +0000

    Feb 24, 2025, 10:14 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -3,14 +3,20 @@
 ### There is a total of 3 bugs in the following program.
 ### Identify and fix the bugs:
 
-score_one = 80
-score_two = 90
-score_three = 75
+# score_one = 80
+# score_two = 90
+# score_three = 75
 
-total = score_one + score_two + score_three
+# total = score_one + score_two + score_three
 
-average_score = total / 3
+# average_score = total / 3
 
-student_name = "Alex"
+# student_name = "Alex"
 
-print("Average score for " + student_name + " is: " + str(average_score))
+# print("Average score for " + student_name + " is: " + str(average_score))
+
+
+
+## Task 1: For Loop: 1 to 10 using range(start, stop)
+
+# Use a 'for' loop to print numbers from 1 to 10.
