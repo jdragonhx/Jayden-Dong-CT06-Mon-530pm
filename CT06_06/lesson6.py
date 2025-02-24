ce2a8cab1cf862411ba730c368aa91171e3f143d
jdragonhx <jayden.dhx@gmail.com>
2025-02-24 08:29:57 +0000

    Feb 24, 2025, 8:29 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -9,5 +9,68 @@
 # **Task 1c**:
 # print("Hello, World!")
 
+
+# numStud = int(input("how many students do you have?"))
+# sumScore = 0
+
+
+# for i in range(numStud):
+#     Score = int(input("how much did you score?"))
+#     sumScore = sumScore + Score
+# print(sumScore/numStud)
+
+
 # **Task 1d**:
-if = 5
+# I = 5
+
+# **Task 1e**:
+# print ("Hello, World!")
+
+
+# **Task 2a**:
+# age = -5
+# print(age)
+
+# **Task 2b**:
+# name = "Alice"
+# print(name)
+
+
+# **Task 2c**:
+# x = 5
+# print(x)
+
+
+# **Task 2d**:
+# print("Hello, World!")
+
+
+# **Task 3a**:
+# age = 25
+# print(age + 1)
+
+
+# **Task 3b**:
+# number = 10
+# print(number - 5)
+
+
+# **Task 3c**:
+# Repeat = 5
+# print("Repeat" * 3)
+
+
+# **Task 3d**:
+# year = 2023
+# print("The year is " + str(year))
+
+
+# **Task 3e**:
+# x = 10
+# y = x / 2
+
+
+# **Task 3f**:
+# end = 5
+# for i in range(end):
+#     print(i)
