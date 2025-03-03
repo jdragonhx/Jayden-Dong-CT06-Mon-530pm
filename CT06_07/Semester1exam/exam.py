1661630d1a3ae23e12d02a1c5c6928a4d976c147
jdragonhx <jayden.dhx@gmail.com>
2025-03-03 09:54:20 +0000

    Mar 3, 2025, 9:54 AM

--- CT06_07/Semester1exam/exam.py
+++ CT06_07/Semester1exam/exam.py
@@ -3,7 +3,7 @@
 # print("nice to meet you " + (name)) # print nice to meet you plus name
 
 # Q2
-start = int(input("what number do you want to start with?"))
+start = int(input("what number do you want to start with?")) #
 end = int(input("what number to you want to end at?"))
 step = int(input("what number do you want to increment by?"))
 
