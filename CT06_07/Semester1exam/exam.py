68a43d088e44c183795fcb253c2f64d2fb0936cc
jdragonhx <jayden.dhx@gmail.com>
2025-03-03 09:45:02 +0000

    Mar 3, 2025, 9:45 AM

--- CT06_07/Semester1exam/exam.py
+++ CT06_07/Semester1exam/exam.py
@@ -3,4 +3,4 @@ name = input("what is you name?") #ask user for name
 print("nice to meet you " + (name)) #print nice to meet you plus name
 
 # Q2
-start = str(input("what "))
+start = str(input("what number do you want to start with?"))
