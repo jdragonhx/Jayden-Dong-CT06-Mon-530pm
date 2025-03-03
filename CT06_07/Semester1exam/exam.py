21d5a0ff782248e258456bf253b047dbdceaf384
jdragonhx <jayden.dhx@gmail.com>
2025-03-03 09:54:41 +0000

    Mar 3, 2025, 9:54 AM

--- CT06_07/Semester1exam/exam.py
+++ CT06_07/Semester1exam/exam.py
@@ -4,7 +4,7 @@
 
 # Q2
 start = int(input("what number do you want to start with?")) #ask user for num they one to start with
-end = int(input("what number to you want to end at?"))
+end = int(input("what number to you want to end at?")) #ask use end num.
 step = int(input("what number do you want to increment by?"))
 
 for i in range(start, end, step):
