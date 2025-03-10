fd4c90b2887ec38763e97900a05ca5a1696374f0
jdragonhx <jayden.dhx@gmail.com>
2025-03-10 10:07:46 +0000

    Mar 10, 2025, 10:07 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -18,3 +18,8 @@ import time
 # asking the user for the number to countdown from, before
 # counting down every second from the number given by the user.
 
+for i in range(10, 0, -1):
+    print(i)
+    time.sleep(1)  #Wait for 1 second between each number
+
+print("liftoff!")  #This will be printed after the countdown
