10dca423fa3c0b5a51834ed70e16bf664e292f26
jdragonhx <jayden.dhx@gmail.com>
2025-03-10 10:49:18 +0000

    Mar 10, 2025, 10:49 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -88,7 +88,7 @@ import random
 # What is 2 + 5? << 7 >>
 # True
 
-
+# yay did everything myself
 num1 = random.randint(1, 50)
 num2 = random.randint(1, 50)
 question = ("what is" + str(num1) + " + " + str(num2) + "?")
