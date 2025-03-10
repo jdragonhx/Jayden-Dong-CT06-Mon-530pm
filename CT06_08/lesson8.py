079c8a15ae80cacc32da8e0015edae2876affbc7
jdragonhx <jayden.dhx@gmail.com>
2025-03-10 10:04:08 +0000

    Mar 10, 2025, 10:04 AM

---
+++ CT06_08/lesson8.py
@@ -0,0 +1,14 @@
+## Task 1: 'time' library
+
+# **Task 1a**:
+# Import the 'time' library and make use of the 'time.sleep()'
+# function to create a 10 seconds countdown timer that counts
+# to 1, printing the number of seconds remaining every second.
+
+
+
+
+# **Task 1b**:
+# Modify your code from Task 1a to include an 'input()' function
+# asking the user for the number to countdown from, before
+# counting down every second from the number given by the user.
