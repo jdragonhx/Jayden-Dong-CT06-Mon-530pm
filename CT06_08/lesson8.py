7175abf4699946c6562c01787bd590180d8b733d
jdragonhx <jayden.dhx@gmail.com>
2025-03-10 10:07:28 +0000

    Mar 10, 2025, 10:07 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -6,14 +6,15 @@ import time
 # function to create a 10 seconds countdown timer that counts
 # to 1, printing the number of seconds remaining every second.
 
-for i in range(10, 0, -1):
-    print(i)
-    time.sleep(1)  #Wait for 1 second between each number
+# for i in range(10, 0, -1):
+#     print(i)
+#     time.sleep(1)  #Wait for 1 second between each number
 
-print("liftoff!")  #This will be printed after the countdown
+# print("liftoff!")  #This will be printed after the countdown
 
 
 # **Task 1b**:
 # Modify your code from Task 1a to include an 'input()' function
 # asking the user for the number to countdown from, before
 # counting down every second from the number given by the user.
+
