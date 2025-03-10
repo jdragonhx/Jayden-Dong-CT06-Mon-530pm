d947a102c74aa0c46790b63fb526373daf88fa45
jdragonhx <jayden.dhx@gmail.com>
2025-03-10 10:10:11 +0000

    Mar 10, 2025, 10:10 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -19,7 +19,7 @@ import time
 # counting down every second from the number given by the user.
 
 int(input("what number do you want to coundown from?"))
-int(input("what number do you"))
+int(input("what number do you want to end with"))
 
 for i in range(10, 0, -1):
     print(i)
