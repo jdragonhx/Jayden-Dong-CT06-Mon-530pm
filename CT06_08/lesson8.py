2c8bab9cb99ccd28f525a8ff8faa247e3cce18d7
jdragonhx <jayden.dhx@gmail.com>
2025-03-10 10:19:47 +0000

    Mar 10, 2025, 10:19 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -47,3 +47,19 @@ import random
 #     num = random.randint(0, 9999)
 #     print(num)
 
+## Task 3: Print Boolean Value & Condition
+
+# **Task 3a**:
+# Assign a boolean value to a variable and print it.
+
+# **Task 3b**:
+# Create 2 variables both holding the "True" boolean.
+# Print out the result of comparing the 2 variables using
+# the "==" operator.
+
+# **Task 3c**:
+# Now, assign 1 variable the "True" boolean, and assign another
+# variable the "False" boolean.
+
+# Print out the result of comparing the 2 variables using
+# the "==" operator.
