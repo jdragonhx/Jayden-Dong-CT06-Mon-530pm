a8f93d86b0a2606e6139d0f6c5e66b607fa9dab3
jdragonhx <jayden.dhx@gmail.com>
2025-03-24 10:42:17 +0000

    Mar 24, 2025, 10:42 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -12,6 +12,6 @@
 
 number = int(input("give me a number: "))
 if number > -1:
-    print(  str(number)) + "is a positive number"
+    print(  str(number) + "is a positive number")
 else:
     print(  str(number) + " is a negative number")
