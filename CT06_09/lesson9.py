fd80332ac3862e9052516c04a58fc15c37d72a07
jdragonhx <jayden.dhx@gmail.com>
2025-03-24 10:03:02 +0000

    Mar 24, 2025, 10:03 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,14 +1,14 @@
 # print("Hello from lesson 9")
 
 # task 3a
-import random
+# import random
 
-somenumber = random.radint(1,10)
-
-
-answer = input("guess a number from 1 to 10")
-answer = int(answer)
+# somenumber = random.radint(1,10)
 
 
-if answer == somenumber:
-    print("congratulations!")
+# answer = input("guess a number from 1 to 10")
+# answer = int(answer)
+
+
+# if answer == somenumber:
+#     print("congratulations!")
