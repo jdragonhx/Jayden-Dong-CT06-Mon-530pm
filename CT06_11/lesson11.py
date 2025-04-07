30538130614a88687ecf71fe3e0852e9a6ce6671
jdragonhx <jayden.dhx@gmail.com>
2025-04-07 10:21:28 +0000

    Apr 7, 2025, 10:21 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,3 +1,7 @@
 # print("Hello from lesson 11")
 
 
+## Task 1: The first run
+# 1. Go to replit and click on the file "main.py"
+# 2. Run the program by clicking on the "Run" button.
+# 3. What do you see in the "Console" on the right side?
