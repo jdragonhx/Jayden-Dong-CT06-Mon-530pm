d038f0b1f69828cb52a47e3229b44fa17c491d3c
jdragonhx <jayden.dhx@gmail.com>
2025-04-07 10:29:55 +0000

    Apr 7, 2025, 10:29 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -6,10 +6,11 @@
 # 2. Run the program by clicking on the "Run" button.
 # 3. What do you see in the "Console" on the right side?
 
-rider1 = 125
-rider2 = 150
+# rider1 = 125
+# rider2 = 150
 
 # if rider1 > 120 and rider2 > 120:
     # print("please proceed to take the ride")
 # else:
     # print("you have to come back when you grow taller")
+
