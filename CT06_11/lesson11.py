9f877a2722572cbe13964eb28ccb38592e976e3d
jdragonhx <jayden.dhx@gmail.com>
2025-04-14 09:58:36 +0000

    Apr 14, 2025, 9:58 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -56,4 +56,6 @@
 
 user_day = input("what day is it today?")
 if user_day == "saturday" or "sunday":
-    print("Hooray! Its the weekend! No  scx")
+    print("Hooray! Its the weekend! No  school")
+else:
+    
