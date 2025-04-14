daeed57d92f0c71522004a0a0c855b5c0da1c414
jdragonhx <jayden.dhx@gmail.com>
2025-04-14 10:04:50 +0000

    Apr 14, 2025, 10:04 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -53,9 +53,18 @@
 # else:
 #     print("nuh uh not a good color")
 
+# Task 8: Not the Right Day (NOT)
+# Create a program that asks the user for the day of the week. If the
+# input is not "Saturday", the program should print "It's not the
+# weekend yet!"
 
-user_day = input("what day is it today?")
-if not user_day == "saturday" and not user_day == "sunday": 
-    print("ITS NOT THE WEEKEND YET GO TO SCHOOL!!!!!")
-else:
-    print("Hooray! Its the weekend! No  school")
+# 1. Ask the user for the day of the week.
+# 2. Using the 'not' operator, check if the input is not "Saturday".
+# 3. If true, print "It's not the weekend yet!"
+
+
+# user_day = input("what day is it today?")
+# if not user_day == "saturday" and not user_day == "sunday": 
+#     print("ITS NOT THE WEEKEND YET GO TO SCHOOL!!!!!")
+# else:
+#     print("Hooray! Its the weekend! No  school")
