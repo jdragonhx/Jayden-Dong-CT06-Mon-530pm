c4c4c2d98d2a731370385899a789dc1574716e7c
jdragonhx <jayden.dhx@gmail.com>
2025-04-14 10:38:30 +0000

    Apr 14, 2025, 10:38 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -8,18 +8,20 @@
 # cover
 # score
 
-word = input("give me a five letter word:  ")
-contains_o = False
-contains_e = False
-for i in word:
-    # print(word)
-    if word == "o":
-        contains_o = True
-    elif word == "e":
-        contains_e = True
+# word = input("give me a five letter word:  ")
+# contains_o = False
+# contains_e = False
+# for i in word:
+#     # print(word)
+#     if word == "o":
+#         contains_o = True
+#     elif word == "e":
+#         contains_e = True
 
-# outside the loop
-if not (contains_o == True) or (contains_e == True):
-    print("invalid word")
-else:
-    print("good word:  "  +  word)
+# # outside the loop
+# if not (contains_o == True) or (contains_e == True):
+#     print("invalid word")
+# else:
+#     print("good word:  "  +  word)
+
+
