ce2bcf5bbfe043bd626bbc66083fcc3850f3bf72
jdragonhx <jayden.dhx@gmail.com>
2025-04-21 10:43:21 +0000

    Apr 21, 2025, 10:43 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,1 +1,12 @@
 print("Hello from lesson 13")
+
+# for entertament
+
+num = 0
+while not num == 4:
+    num = random.randint(1, 100000000)
+    print(num)
+    if num == 4:
+        break
+else:
+    print("end of loop")
