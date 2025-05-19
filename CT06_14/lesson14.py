43dfc48c0689ab24266e539d86e98cbb4cc368b9
jdragonhx <jayden.dhx@gmail.com>
2025-05-19 10:41:34 +0000

    May 19, 2025, 10:41 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -32,7 +32,8 @@ randomY = random.randint(-100, 100)
 
 window.mainloop() # is a must be the last line
 
-newguy = turtle
+newguy = turtle.Turtle()
+# newguy
 
 # for i in range(6):
 #     for inside in range(3):
