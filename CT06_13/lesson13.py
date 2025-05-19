6b5b09b74171d7bb4e0f01b29f1df36760efbaa5
jdragonhx <jayden.dhx@gmail.com>
2025-05-19 08:42:20 +0000

    May 19, 2025, 8:42 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,33 +1,69 @@
 print("Hello from lesson 13")
 
-# for entertament
-import random
-num = 0
-while not num == 4:
-    num = random.randint(1, 1000000000) # one billion
-    print(num)
-    if num == 4:
-        break
+# # for entertament
+# import random
+# num = 0
+# while not num == 4:
+#     num = random.randint(1, 1000000000) # one billion
+#     print(num)
+#     if num == 4:
+#         break
+# else:
+#     print("end of loop")
+
+
+# # recap Q1
+# # ATM Sim
+# balence = 1000
+# while True:
+#     print("Welcome to your bank! ")
+#     print("chose the following:")
+#     print("1. Withdraw Cash")
+#     print("2. Deposit Cash")
+#     print("3. Show Balence")
+#     print("4. Exit")
+#     reply = input("")
+#     reply = int(reply)
+#     if reply == 4:
+#         print("Thank you for banking with us! ")
+#         break
+#     elif reply == 1:
+#         answer = input("how much to withdraw? ")
+#         answer = int(answer)
+#         break
+
+# grocerys = ["apple", "bread" , "carrots", "dates", "eggs", "herbs"]
+# print(grocerys[5])
+
+# for name in grocerys:
+#     print(name)
+
+# grocerys.pop()
+# for name in grocerys:
+#     print(name)
+
+# del(grocerys[1])
+
+# for name in grocerys:
+#     print(name)
+
+
+# for item in grocerys:
+#     if item == "Apples":
+#         print(item + ": I need 5 of these")
+#     if 
+
+
+#test q1 (revision)
+while counter < 150:
+    print(counter)
+    counter = counter + 12
 else:
-    print("end of loop")
+    print("end of list")
 
-
-# recap Q1
-# ATM Sim
-balence = 1000
-while True:
-    print("Welcome to your bank! ")
-    print("chose the following:")
-    print("1. Withdraw Cash")
-    print("2. Deposit Cash")
-    print("3. Show Balence")
-    print("4. Exit")
-    reply = input("")
-    reply = int(reply)
-    if reply == 4:
-        print("Thank you for banking with us! ")
-        break
-    elif reply == 1:
-        answer = input("how much to withdraw? ")
-        answer = int(answer)
-        break
+secret = "blastoff!"
+answer = input("tell me today's password? ")
+if answer == secret:
+    print("its matched")
+else:   
+    print("go away i will tell the police!")
---
+++ EndSem.py
@@ -0,0 +1,40 @@
+# Task 1
+# Print numbers from 10 to 200 using the while loop
+# Your numbers must be in multiples of 10.
+# 10 must be first number printed, and 200 the last number.
+
+# Example: 10, 20, 30 ..... 180, 190, 200.
+# Note that the numbers do not need to be printed in one line.
+# Write your code here
+# counter = 1
+# while counter < 201:
+#     print(counter)
+#     counter += 1
+
+
+# Task 2
+# Code a password checker to protect your code!
+
+# Assign a password "superpass123" to a variable
+# Ask the user to enter a password
+# If the password matches, print “Access Granted”
+# If the password does not match, print “Access Denied”
+
+# password = "superpass123"
+# user_enter = input("please enter the password")
+# if user_enter == password:
+#     print("Access Granted!")
+# else:
+#     print("Acess Denied")
+
+
+# Q2
+# Task 1
+planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
+print(planets[2])
+
+planets.append("neptune")
+print(planets)
+planets.pop(3)
+planets.insert(3, "muskworld")
+print(planets)
