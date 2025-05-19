1abcb3176f2f5e8063db4f569d59adc294b3cb0b
jdragonhx <jayden.dhx@gmail.com>
2025-05-19 10:17:05 +0000

    May 19, 2025, 10:17 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -1,1 +1,20 @@
-print("Hello from lesson 15")
+import turtle
+window = turtle.Screen()
+window.setup(width = 600, height = 400)
+
+# for i in range(6):
+#     for inside in range(3):
+
+#         turtle.forward(55)
+#         turtle.left(120)
+
+#     turtle.right(60)
+
+# window.mainloop()
+
+daisy = turtle.Turtle()
+daisy.shape("turtle")
+daisy.color("red")
+daisy.forward(50)
+
+tom = turtle 
