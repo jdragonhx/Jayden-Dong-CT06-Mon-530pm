2d047a54a37fb04d1cc1df60e06e749b4b301267
jdragonhx <jayden.dhx@gmail.com>
2025-07-21 11:44:56 +0000

    Jul 21, 2025, 11:44 AM

--- Code_Hero_Championship/task.py
+++ Code_Hero_Championship/task.py
@@ -1,1 +1,25 @@
-scores = [82, 75, 90, 75, 66, 90, 88, 95, 82, 91, 95, 70, 66, 84, 89, 92, 91, 88, 90, 85]
+
+import random
+def roast_move(move):
+  roasts = [
+      f"Wow, '{move}' - bold choice... said no grandmaster ever.",
+          f"'{move}' - aka the 'I give up' move.",
+              f"Did '{move}' come from a chess book... for beginners?"
+                ]
+                  return random.choice(roasts)
+                  def predict_loss(moves_left):
+                    predictions = [
+                        f"Checkmate in {moves_left}... if you don't resign sooner.",
+                            f"You've got {moves_left} moves left - barely enough time to facepalm.",
+                                f"Game over in {moves_left} moves - better call a timeout... for your ego."
+                                  ]
+                                    return random.choice(predictions)
+                                    def pawn_pwnz():
+                                      print("Welcome to PawnPwnz - your chess game just got ROASTED!")
+                                        move = input("Enter your move: ")
+                                          print(roast_move(move))
+                                            moves_left = int(input("How many moves till checkmate (guess): "))
+                                              print(predict_loss(moves_left))
+                                              pawn_pwnz()
+                                              ```
+                              
